8a1732c4c652f5c2ac2fb28161565c62fd72d0b5
mpawelm <mozola.pawel@gmail.com>
2019-02-02 00:45:26 +0100

    Basic DDD Objects

---
+++ src/api/Shared/CRP.Shared.BaseDomainObjects/Entity.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+using MediatR;
+
+namespace CRP.Shared.BaseDomainObjects
+{
+    public abstract class Entity
+    {
+        private int? requestedHashCode;
+
+        private List<INotification> domainEvents;
+
+        public virtual int Id { get; protected set; }
+
+        public IReadOnlyCollection<INotification> DomainEvents => this.domainEvents?.AsReadOnly();
+
+        public static bool operator ==(Entity left, Entity right)
+        {
+            if (object.Equals(left, null))
+            {
+                return object.Equals(right, null);
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Entity left, Entity right)
+        {
+            return !(left == right);
+        }
+
+        public void AddDomainEvent(INotification eventItem)
+        {
+            this.domainEvents = this.domainEvents ?? new List<INotification>();
+            this.domainEvents.Add(eventItem);
+        }
+
+        public void RemoveDomainEvent(INotification eventItem)
+        {
+            this.domainEvents?.Remove(eventItem);
+        }
+
+        public void ClearDomainEvents()
+        {
+            this.domainEvents?.Clear();
+        }
+
+        public bool IsTransient()
+        {
+            return this.Id == default(int);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Entity))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var item = (Entity)obj;
+
+            if (item.IsTransient() || this.IsTransient())
+            {
+                return false;
+            }
+
+            return item.Id == this.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            if (!this.IsTransient())
+            {
+                if (!this.requestedHashCode.HasValue)
+                {
+                    this.requestedHashCode = this.Id.GetHashCode() ^ 31;
+                }
+
+                return this.requestedHashCode.Value;
+            }
+
+            return base.GetHashCode();
+        }
+    }
+}
