aa4f3e79314b9052db0ae1e89f654800e8a4dfe8
haushinka22 <haushinka22@gmail.com>
2019-03-02 14:49:14 +0100

    register

--- src/web/car-rental/src/app/app-routing.module.ts
+++ src/web/car-rental/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FleetLayoutComponent } from './shared/components/fleet-layout/fleet-layout.component';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { RegisterComponent } from './modules/auth/pages/register/register.component';
 
 const routes: Routes = [{
   component: FleetLayoutComponent,
@@ -11,7 +13,18 @@ const routes: Routes = [{
   component: FleetLayoutComponent,
   path: '',
   loadChildren: './modules/rental/rental.module#RentalModule'
-}];
+},
+{
+  component: LoginComponent,
+  path: 'login'
+},
+{
+  component: RegisterComponent,
+  path: 'register'
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/web/car-rental/src/app/app.module.ts
+++ src/web/car-rental/src/app/app.module.ts
@@ -8,17 +8,28 @@ import { AppMaterialModule } from './shared/modules/app-material/app-material.mo
 import  { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FleetLayoutComponent } from './shared/components/fleet-layout/fleet-layout.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { LoginComponent } from "./modules/auth/pages/login/login.component";
+import { RegisterComponent} from "./modules/auth/pages/register/register.component";
+ 
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FleetLayoutComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent ,
+    RegisterComponent
+
   ],
   imports: [
     BrowserAnimationsModule,
     AppRoutingModule,
     AppMaterialModule
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
